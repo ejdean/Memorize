f860914b80803b06f1fe71277091887656a05721
Edward Dean <ejdean6@gmail.com>
2020-11-26 15:01:39 +0000

    Building out the model and view model

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,12 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    var viewModel: EmojiMemoryGame
+    
     var body: some View {
-                HStack {
-                    ForEach(0..<4) { index in
-                        CardView(isFaceUp: false)
+                VStack {
+                    ForEach(viewModel.cards) { card in
+                        CardView(card: card).onTapGesture {
+                            viewModel.choose(card: card)
+                            
+                        }
+                    }
                 }
-            }
             .padding()
             .foregroundColor(Color.orange)
             .font(Font.largeTitle)
@@ -21,13 +26,14 @@ struct ContentView: View {
 }
 
 struct CardView: View {
-    var isFaceUp: Bool
+    var card: MemoryGame<String>.Card
+    
     var body: some View {
         ZStack {
-            if isFaceUp {
+            if card.isFaceUp {
                 RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
                 RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
-                Text("👻")
+                Text(card.content)
             } else {
                 RoundedRectangle(cornerRadius: 10.0).fill()
             }
@@ -40,7 +46,7 @@ struct CardView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            ContentView()
+            ContentView(viewModel: EmojiMemoryGame())
         }
     }
 }
