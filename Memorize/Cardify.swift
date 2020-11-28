5d5581b2d718dd7c859fb548192aef238a076bce
Edward Dean <ejdean6@gmail.com>
2020-11-28 08:27:36 +0000

    Implemented the view modifier 'cardify'

---
+++ Memorize/Cardify.swift
@@ -0,0 +1,34 @@
+//
+//  Cardify.swift
+//  Memorize
+//
+//  Created by Edward Dean on 28/11/2020.
+//
+
+import SwiftUI
+
+struct Cardify: ViewModifier {
+    var isFaceUp: Bool
+    
+    func body(content: Content) -> some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
+                RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
+                content
+            } else {
+                RoundedRectangle(cornerRadius: cornerRadius).fill()
+            }
+        }
+    }
+    // MARK: - Drawing Constants Cardify
+
+    private let cornerRadius: CGFloat = 10
+    private let edgeLineWidth: CGFloat = 3
+}
+
+extension View {
+    func cardify(isFaceUp: Bool) -> some View {
+        self.modifier(Cardify(isFaceUp: isFaceUp))
+    }
+}
